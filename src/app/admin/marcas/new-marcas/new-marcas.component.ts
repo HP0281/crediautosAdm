d429759fd46484b945afd4d6d5fefe2549e32f15
Harold.parra <Harold.parra@t-evolvers.com>
2021-11-14 20:36:34 -0500

    marcas ok

--- src/app/admin/marcas/new-marcas/new-marcas.component.ts
+++ src/app/admin/marcas/new-marcas/new-marcas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MarcasService } from 'src/app/services/marcas.service';
 
 @Component({
   selector: 'app-new-marcas',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-marcas.component.css']
 })
 export class NewMarcasComponent implements OnInit {
+  marcaForm: FormGroup;
 
-  constructor() { }
+  constructor(private marcaService: MarcasService, private fb: FormBuilder,
+    private dialog: NgbModal) {
+    this.initForm();
+   }
 
   ngOnInit(): void {
   }
+  
+  onGuardar(modal){
+    if (this.marcaForm.valid) {
+      this.marcaService.onSaveMarca(this.marcaForm.value, null);
+      this.dialog.open(modal);
+      this.marcaForm.reset();
+    }
+  }
 
+  initForm(){
+    this.marcaForm = this.fb.group({
+      name: new FormControl('', [Validators.required])
+    })
+  }
 }
