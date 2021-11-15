81e752a3dc10905940434b5937bb7fd2dc5bcf4c
Harold.parra <Harold.parra@t-evolvers.com>
2021-11-15 12:23:45 -0500

    modelos ok

--- src/app/admin/modelos/new-modelos/new-modelos-routing.module.ts
+++ src/app/admin/modelos/new-modelos/new-modelos-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewModelosComponent } from './new-modelos.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: "", component: NewModelosComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/modelos/new-modelos/new-modelos.component.ts
+++ src/app/admin/modelos/new-modelos/new-modelos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MarcasService } from 'src/app/services/marcas.service';
+import { ModelosService } from 'src/app/services/modelos.service';
 
 @Component({
   selector: 'app-new-modelos',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewModelosComponent implements OnInit {
 
-  constructor() { }
+  marcas: any[];
+  modeloForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private marcaService: MarcasService,
+    private modelosService: ModelosService) { 
+    this.marcaService.marcas.subscribe((resp:any)=>{
+      this.marcas = resp;
+      console.log(resp)
+    })
+    this.initForm();
+  }
 
   ngOnInit(): void {
   }
 
+  initForm(){
+    this.modeloForm = this.fb.group({
+      marca: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required])
+    })
+  }
+  onGuardar(){
+    if (this.modeloForm.valid) {
+      this.modelosService.onSaveModelo(this.modeloForm.value, null);
+    }
+  }
 }
